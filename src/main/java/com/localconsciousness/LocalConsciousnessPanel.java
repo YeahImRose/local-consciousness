fa582d797ee17b4e9b3d0deb9974545b9415210c
rose <awoonar.dust@gmail.com>
2025-09-19 16:59:57 -0400

    Rework panel, based largely on BossingInfo

--- src/main/java/com/localconsciousness/LocalConsciousnessPanel.java
+++ src/main/java/com/localconsciousness/LocalConsciousnessPanel.java
@@ -2,103 +2,89 @@ package com.localconsciousness;
 
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.Client;
+import net.runelite.api.gameval.InterfaceID;
+import net.runelite.api.widgets.WidgetInfo;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.ui.ColorScheme;
 import net.runelite.client.ui.PluginPanel;
+import net.runelite.client.ui.components.PluginErrorPanel;
 
 
+import javax.inject.Inject;
 import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.border.CompoundBorder;
 import javax.swing.border.EmptyBorder;
+import javax.swing.border.MatteBorder;
 import java.awt.*;
 
 @Slf4j
 public class LocalConsciousnessPanel extends PluginPanel {
+    @Inject
+    ConfigManager configManager;
+
     protected final LocalConsciousnessPlugin plugin;
+    protected final LocalConsciousnessConfig config;
 
-    JButton searchButton;
-
-    JLabel scaleLabel;
-    JSpinner scaleSpinner;
-    SpinnerNumberModel scaleSpinnerModel;
-
-    JLabel speedLabel;
-    JSpinner speedSpinner;
-    SpinnerNumberModel speedSpinnerModel;
-
-    JLabel opacityLabel;
-    JSpinner opacitySpinner;
-    SpinnerNumberModel opacitySpinnerModel;
+    private final JPanel fullPanel = new JPanel();
+    private final JPanel titlePanel = new JPanel();
+    private final JPanel currentItemPanel = new JPanel();
+    private final JPanel buttonsPanel = new JPanel();
+    private final JPanel optionsPanel = new JPanel();
 
     JLabel currentItemLabel;
     JLabel currentItemName;
 
+    JButton searchButton;
+    JButton randomButton;
+
+    JLabel scaleLabel;
+    JSpinner scaleSpinner;
+    SpinnerNumberModel scaleSpinnerModel;
+    JLabel speedLabel;
+    JSpinner speedSpinner;
+    SpinnerNumberModel speedSpinnerModel;
+    JLabel opacityLabel;
+    JSpinner opacitySpinner;
+    SpinnerNumberModel opacitySpinnerModel;
+
 
     public LocalConsciousnessPanel(Client client, LocalConsciousnessConfig config, final LocalConsciousnessPlugin plugin, ConfigManager configManager) {
         super();
-
         this.plugin = plugin;
+        this.config = config;
 
-        setBorder(new EmptyBorder(18, 10, 0, 10));
-        setBackground(ColorScheme.DARK_GRAY_COLOR);
-        setLayout(new GridBagLayout());
+        setPanelStaticLabels();
+        this.fullPanel.add(buildTitlePanel());
+        //this.fullPanel.add(buildCurrentItemPanel());
+        this.fullPanel.add(buildButtonsPanel());
+        this.fullPanel.add(buildOptionsPanel());
 
-        GridBagConstraints c = new GridBagConstraints();
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.gridx = 0;
-        c.gridy = 0;
-        c.weightx = 1;
-        c.weighty = 0;
-        c.insets = new Insets(0, 0, 10, 0);
+        this.setLayout(new BorderLayout());
+        this.setBorder(new EmptyBorder(10, 10, 10, 10));
+        this.fullPanel.setLayout(new GridLayout(10, 0, 0, 10));
+        this.add(fullPanel);
+    }
 
-        this.scaleLabel = new JLabel("Icon Scale:");
-        add(scaleLabel, c);
+    private JPanel buildTitlePanel() {
+        titlePanel.setBorder(new CompoundBorder(new EmptyBorder(5, 0, 0, 0), new MatteBorder(0, 0, 1, 0, new Color(37, 125, 141))));
+        titlePanel.setLayout(new BorderLayout());
+        PluginErrorPanel errorPanel = new PluginErrorPanel();
+        errorPanel.setBorder(new EmptyBorder(2, 0, 3, 0));
+        errorPanel.setContent("Local Consciousness", "");
+        titlePanel.add(errorPanel, "Center");
+        return titlePanel;
+    }
 
-        c.gridx++;
+    private JPanel buildCurrentItemPanel() {
+        //currentItemPanel.setBorder();
+        //currentItemPanel.setLayout(new BorderLayout());
 
-        this.scaleSpinnerModel = new SpinnerNumberModel(config.size(), 1, 5000, 1);
-        this.scaleSpinner = new JSpinner(scaleSpinnerModel);
-        scaleSpinner.addChangeListener(e -> configManager.setConfiguration("localconsciousness", "size", (int) scaleSpinner.getValue()));
-        add(scaleSpinner, c);
+        return currentItemPanel;
+    }
 
-        c.gridy++;
-        c.gridx = 0;
-
-        this.speedLabel = new JLabel("Icon Speed:");
-        add(speedLabel, c);
-
-        c.gridx++;
-
-        this.speedSpinnerModel = new SpinnerNumberModel(config.speed(), 1, 1000, 1);
-        this.speedSpinner = new JSpinner(speedSpinnerModel);
-        speedSpinner.addChangeListener(e -> configManager.setConfiguration("localconsciousness", "speed", (int) speedSpinner.getValue()));
-        add(speedSpinner, c);
-
-        c.gridy++;
-        c.gridx = 0;
-
-        this.opacityLabel = new JLabel("Icon Opacity:");
-        add(opacityLabel, c);
-
-        c.gridx++;
-
-        this.opacitySpinnerModel = new SpinnerNumberModel(config.opacity(), 0, 100, 5);
-        this.opacitySpinner = new JSpinner(opacitySpinnerModel);
-        opacitySpinner.addChangeListener(e -> configManager.setConfiguration("localconsciousness", "opacity", (int) opacitySpinner.getValue()));
-        add(opacitySpinner, c);
-
-        c.gridy++;
-        c.gridx = 0;
-
-        this.currentItemLabel = new JLabel("Current Item:");
-        add(currentItemLabel, c);
-
-        c.gridx++;
-
-        this.currentItemName = new JLabel("");
-        add(currentItemName, c);
-
-        c.gridy++;
-        c.gridx = 0;
+    private JPanel buildButtonsPanel() {
+        this.buttonsPanel.setLayout(new GridLayout(2, 1, 2, 5));
 
         this.searchButton = new JButton("Search For Item");
         searchButton.setFocusable(false);
@@ -107,8 +93,52 @@ public class LocalConsciousnessPanel extends PluginPanel {
             plugin.updateFromSearch();
             searchButton.setFocusable(true);
         });
-        c.gridwidth = 2;
-        add(searchButton, c);
+
+        this.randomButton = new JButton("Randomize Item");
+        randomButton.setFocusable(false);
+        randomButton.addActionListener(e -> {
+            randomButton.setFocusable(false);
+            plugin.randomizeItem();
+            randomButton.setFocusable(true);
+        });
+        this.buttonsPanel.add(this.searchButton);
+        this.buttonsPanel.add(this.randomButton);
+
+        return buttonsPanel;
+    }
+
+    private JPanel buildOptionsPanel() {
+        this.optionsPanel.setLayout(new GridLayout(3, 2, 2, 10));
+
+        this.scaleSpinnerModel = new SpinnerNumberModel(config.size(), 1, 5000, 1);
+        this.scaleSpinner = new JSpinner(scaleSpinnerModel);
+        scaleSpinner.addChangeListener(e -> configManager.setConfiguration("localconsciousness", "size", (int) scaleSpinner.getValue()));
+
+        this.speedSpinnerModel = new SpinnerNumberModel(config.speed(), 1, 1000, 1);
+        this.speedSpinner = new JSpinner(speedSpinnerModel);
+        speedSpinner.addChangeListener(e -> configManager.setConfiguration("localconsciousness", "speed", (int) speedSpinner.getValue()));
+
+        this.opacitySpinnerModel = new SpinnerNumberModel(config.opacity(), 0, 100, 5);
+        this.opacitySpinner = new JSpinner(opacitySpinnerModel);
+        opacitySpinner.addChangeListener(e -> configManager.setConfiguration("localconsciousness", "opacity", (int) opacitySpinner.getValue()));
+
+        this.optionsPanel.add(this.scaleLabel);
+        this.optionsPanel.add(this.scaleSpinner);
+
+        this.optionsPanel.add(this.speedLabel);
+        this.optionsPanel.add(this.speedSpinner);
+
+        this.optionsPanel.add(this.opacityLabel);
+        this.optionsPanel.add(this.opacitySpinner);
+
+        return optionsPanel;
+    }
+
+    private void setPanelStaticLabels() {
+        this.scaleLabel = new JLabel("Icon Scale: ");
+        this.speedLabel = new JLabel("Icon Speed:");
+        this.opacityLabel = new JLabel("Icon Opacity:");
+        this.currentItemLabel = new JLabel("Current Item:");
 
     }
 
